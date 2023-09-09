750b41bcc818e7c4684b21bdd4a8dddb450ab072
dmartin07 <dariomartinbenavidez@hotmail.com>
2023-09-09 10:44:03 -0300

    enrutamientoListo

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './Routes/Home';
+import Contact from './Routes/Contact';
+import Detail from './Routes/Detail';
+import Favs from './Routes/Favs';
+import NotFound from './Routes/NotFound';
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} exact>
+          <Route path="home" element={<Home />} />
+          <Route path="dentist/:id" element={<Detail />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="favs" element={<Favs />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
